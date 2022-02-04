684e50060a103361e620c45c36b5a06c72921665
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2022-02-03 19:52:41 -0500

    clean up

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -36,8 +36,8 @@ export default function Header() {
             >
               <path
                 strokeLinecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               ></path>
             </svg>
@@ -57,8 +57,8 @@ export default function Header() {
             >
               <path
                 strokeLinecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M13 10V3L4 14h7v7l9-11h-7z"
               ></path>
             </svg>
