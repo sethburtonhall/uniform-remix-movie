b77f66194fe7262ba12daa850c7daf84b750dc88
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2022-02-03 19:48:21 -0500

    clean up, new manifest,

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -35,7 +35,7 @@ export default function Header() {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                stroke-linecap="round"
+                strokeLinecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
                 d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
@@ -56,7 +56,7 @@ export default function Header() {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                stroke-linecap="round"
+                strokeLinecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
                 d="M13 10V3L4 14h7v7l9-11h-7z"
